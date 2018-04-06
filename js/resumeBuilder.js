f0ac1dddb25304459952a76af624356c0de5e73e
Vamsi Krishna <vamsikrishna.battula@gmail.com>
2018-04-06 12:32:27 +0530

    added Name and role

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,13 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+var name= "Vamsi Krishna";
+var role= "Web Developer";
+
+/* Name */
+var myName = HTMLheaderName.replace("%data%", name);
+$("#header").append(myName);
+
+/*Role*/
+var myRole = HTMLheaderRole.replace("%data%", role);
+$("#header").append(myRole);
